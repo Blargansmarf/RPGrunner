71d61ae2f901fddf59b7c2c71984324c81a2d5a8
Blargansmarf <joshpicarazzi@gmail.com>
2014-06-25 12:28:28 -0500

    also item stuff?

--- RPGrunner/RPGrunner/Item.cs
+++ RPGrunner/RPGrunner/Item.cs
@@ -13,5 +13,46 @@ namespace RPGrunner
 {
     class Item
     {
+        GraphicsDeviceManager graphics;
+        ContentManager content;
+
+        Texture2D itemSpriteSheet;
+        Vector2 dimensions;
+        Vector2 loc;
+
+        List<string> buffs;
+
+        public float minDmg, maxDmg;
+        public float atkSpd;
+
+        enum StatType { intel, dex, str };
+        enum WeaponType { magic, pierce, slash, blunt };
+
+        StatType statType;
+        WeaponType weaponType;
+
+        public Item(GraphicsDeviceManager g, ContentManager c, int weapon)
+        {
+            graphics = g;
+            content = c;
+
+            if (weapon == 0)
+            {
+                LoadContent("Sword");
+
+                minDmg = 1f;
+                maxDmg = 5f;
+
+                statType = StatType.str;
+                weaponType = WeaponType.slash;
+
+                atkSpd = 1f;
+            }
+        }
+
+        private void LoadContent(string sSheet)
+        {
+            itemSpriteSheet = content.Load<Texture2D>(sSheet);
+        }
     }
 }
